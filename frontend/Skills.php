5dc60045dc403b0f2583c3722313fb32b63cef47
G1-Eric-Li <ericlitong@gmail.com>
2022-10-16 17:02:36 +0800

    changed pages to PHP
    shifted old html pages to depreciated folder, added new php pages for roles skills courses
    to be done: adding more behavior/filtering/test datas

---
+++ frontend/Skills.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap 5 Website Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'ljps';
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT skill_id, skill_name FROM skill";
+    $result = $conn->query($sql);
+    ?>
+
+
+    <div class="p-5 bg-primary text-white text-center">
+        <h1>Learning Journey System</h1>
+    </div>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="homepage.html">My Learning Journey</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="HR.html">HR</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+                HR
+            </span>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <h1>Skills</h1>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <th>Skill Id</th>
+                <th>Skill Name</th>
+                <th>Add to Learning Journey</th>
+            </thead>
+
+            <tbody>
+                <?php
+            while ($row = $result->fetch_assoc()) {
+              echo "<tr>" .
+                      "<td>" . $row['skill_id'] . "</td>" .
+                      "<td>" . $row['skill_name'] . "</td>" .
+                      "<td>" . "<a href='Courses.php' class='btn btn-dark' role='button'>Start</a>" . "</td>" .
+                      "</tr>";
+            }
+          ?>
+            </tbody>
+
+        </table>
+    </div>
+
+    <div class="mt-5 p-4 bg-dark text-white text-center">
+        <p>Footer</p>
+    </div>
+
+</body>
+
+</html>
