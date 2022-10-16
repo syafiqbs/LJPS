29b2b36f75b3992caffe0b48af68f506e8859b44
theycallmeeuan <euanchng.2020@scis.smu.edu.sg>
2022-10-16 17:51:13 +0800

    delete skill page

---
+++ frontend/DeleteSkill.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+<div class="p-5 bg-primary text-white text-center">
+        <h1>Learning Journey System</h1>
+</div>
+      
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="homepage.html">My Learning Journey</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="HR.html">HR</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          HR 
+        </span>
+      </div>
+    </nav>
+
+
+    <div class="container mt-5">
+
+    
+    </div>
+
+
+
+
+</body>
+</html>
