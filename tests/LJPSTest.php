114966f1e86e0c7fa8ac5b8b29c7109fcd683999
syafiqbs <cross.symbol@hotmail.com>
2022-11-06 15:31:28 +0800

    testCreateJob() testing

--- tests/LJPSTest.php
+++ tests/LJPSTest.php
@@ -1,13 +1,23 @@
 <?php
     require_once "C:\wamp64\www\SPM\LJPS\backend\Job.php";
+    require_once "C:\wamp64\www\SPM\LJPS\backend\JobDAO.php";
 
     class LJPSTest extends \PHPUnit\Framework\TestCase {
         
-        public function testCreate(){
+        public function testGetJobName(){
             $job = new Job("999", "Swavek", "lorem");
             $job->setJobName('Eric');
             $this->assertEquals($job->getJobName(),'Eric');
         }
+
+        public function testCreateJob(){
+            $job = new Job("999", "Swavek", "lorem");
+            $dao = new JobDAO();
+            $status = $dao->create($job);
+            $this->assertEquals($status, true);
+        }
+
+        
     }
 
 ?>
