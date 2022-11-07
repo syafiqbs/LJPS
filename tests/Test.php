ed5b6276a1129da476af79118a73cee46b8efdb5
G1-Eric-Li <ericlitong@gmail.com>
2022-11-07 17:54:03 +0800

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -18,6 +18,12 @@
         public function testGetJobId(){
             $job = new Job("999", "Swavek", "lorem");
             $job->setJobId('900');
+
+            self::assertStringContainsString(
+                "test",
+                "test"
+            );
+
             $this->assertEquals($job->getJobId(),'900');
         }
 
