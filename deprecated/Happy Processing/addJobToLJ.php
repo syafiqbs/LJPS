e425c66c8cd2b9d58e1edfb51bbaeaa98ff9fe79
G1-Eric-Li <ericlitong@gmail.com>
2022-11-08 17:28:28 +0800

    Delete Depreciated

--- deprecated/Happy Processing/addJobToLJ.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "";
-    var_dump($_POST);
-    $_SESSION['job_id'] = $_POST['job_id'];
-    // add job to user's learning journey list 
-    header("Location: Skills.php");
-
-?>
